c6b3f493b864332112dbf96b09538d0a6c3961af
Britain Southwick <britainjs@gmail.com>
2016-04-18 13:58:59 -0700

    Added a db connection and app run command to the app.

--- friendbuy-prelaunch.py
+++ friendbuy-prelaunch.py
@@ -4,3 +4,10 @@ import sqlite3
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('SETTINGS', silent=True)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    app.run()
