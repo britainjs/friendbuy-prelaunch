f29cc12932e3c47236315b396ab44fb174397208
Britain Southwick <britainjs@gmail.com>
2016-04-18 17:51:31 -0700

    Added a query for existing emails.

--- friendbuy_prelaunch.py
+++ friendbuy_prelaunch.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, g, redirect, url_for
 from contextlib import closing
 import sqlite3
+import mandrill
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_object('settings')
 app.config.from_envvar('SETTINGS', silent=True)
+mandrill_client = mandrill.Mandrill(app.config['MANDRILL_PASSWORD'])
 
 
 def connect_db():
@@ -18,11 +20,33 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 
 @app.route('/', methods=['POST', 'GET'])
 def welcome():
 
+    if request.method == 'POST':
+        email = request.form['email']
+        query = g.db.execute('select email from emails where email = ?', [email])
+        results = [row[0] for row in query.fetchall()]
+        if len(results) > 0:
+            return redirect(url_for('share'))
+
     return render_template('welcome.html')
+
+
+@app.route('/share')
+def share():
+    return "Not Implemented"
 
 if __name__ == '__main__':
     app.run()
