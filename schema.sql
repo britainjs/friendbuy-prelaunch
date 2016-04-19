af38bd2d95df3f4371a914301f3b2db2b92a0753
Britain Southwick <britainjs@gmail.com>
2016-04-19 11:48:12 -0700

    Added confirmation token to the schema.

--- schema.sql
+++ schema.sql
@@ -2,5 +2,6 @@ drop table if exists emails;
 create table emails(
   id integer primary key autoincrement,
   email text not null unique,
-  validated boolean not null
+  validated boolean not null,
+  confirmation_token text not null unique
 );
